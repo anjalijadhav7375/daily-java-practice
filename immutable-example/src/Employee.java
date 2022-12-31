5936ea723360459dfe9e60dfcb1cc70428087b48
Dell <anjalijadhav753@gmail.com>
2022-12-31 15:04:46 +0530

    Adding immutable-example module

---
+++ immutable-example/src/Employee.java
@@ -0,0 +1,39 @@
+public class Employee {
+    String name;
+    int salary;
+    int id;
+    Address address;
+
+    public Employee(String name, int salary, int id, Address address) {
+        this.name = name;
+        this.salary = salary;
+        this.id = id;
+        this.address = new Address(address.flatNum,address.lane1,address.lane2,address.city,address.postCode);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Address getAddress() {
+        return new Address(address.flatNum,address.lane1,address.lane2,address.city,address.postCode);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", id=" + id +
+                ", adress=" + address +
+                '}';
+    }
+}
---
+++ immutable-example/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Anjali",1000,101,new Address
+                ("A-106",null,null,"Pune",42307));
+        System.out.println(employee);
+
+        employee.getAddress().setCity("mumbai");
+        System.out.println(employee);
+
+    }
+}
